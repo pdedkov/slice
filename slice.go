60fe8573ef39e94313d7b54d4bff7df2faaf4125
Pavel E. Dedkov <pavel.dedkov@gmail.com>
2017-03-02 14:08:27 +0300

    src

---
+++ slice.go
@@ -0,0 +1,20 @@
+package slice
+
+import (
+	"reflect"
+	"errors"
+)
+
+func Filter(s interface{}, fn func(interface{}) bool) ([]interface{}, error) {
+	val := reflect.ValueOf(s)
+	if val.Kind() != reflect.Slice {
+		return nil, errors.New("Invalid argument. Given non-slice type argument")
+	}
+	var r []interface{}
+	for i := 0; i < val.Len(); i++ {
+		if (fn(val.Index(i).Interface())) {
+			r = append(r, val.Index(i).Interface())
+		}
+	}
+	return r, nil
+}
---
+++ slice_test.go
@@ -0,0 +1,24 @@
+package slice
+
+import "testing"
+
+func TestFilter(t *testing.T) {
+	val := []string{"", "1", "2"}
+	fn := func (val interface{}) bool {
+		switch val.(type) {
+		case string:
+			if len(val.(string)) > 0 {
+				return true
+			} else {
+				return false
+			}
+		default:
+			return false
+		}
+	}
+
+	arr, _ := Filter(val, fn)
+	if len(arr) != 2 {
+		t.Errorf("fail %+v\n", arr)
+	}
+}
